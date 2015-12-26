8e3a4c9a04bf684a6a93eb1f2b03df6a3d5ee5d9
evrenkaya <evrenk29@gmail.com>
2015-12-26 15:28:08 -0500

    Remove testing code and main method

--- src/main/java/jds/BulkArrayDeque.java
+++ src/main/java/jds/BulkArrayDeque.java
@@ -47,49 +47,4 @@ public class BulkArrayDeque<T> extends ArrayDeque<T> {
 		headIndex = 0;
 		n = backingArray.length;
 	}
-	
-	public static void doIt(BufferedReader r, PrintWriter w){
-		// Testing code
-		BulkArrayDeque<String> deque = new BulkArrayDeque<>(String.class);
-		for (int i = 0; i < 100; i++) {
-			deque.add(i, Integer.toString(i));
-		}
-		
-		deque.removeRange(0, 100);
-		/*System.out.println("SIZE: " + deque.size());
-		for (int i = 0; i < deque.size(); i++) {
-			System.out.println(deque.get(i));
-		}*/
-	}
-	
-	
-	/**
-	 * The driver.  Open a BufferedReader and a PrintWriter, either from System.in
-	 * and System.out or from filenames specified on the command line, then call doIt.
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		try {
-			BufferedReader r;
-			PrintWriter w;
-			if (args.length == 0) {
-				r = new BufferedReader(new InputStreamReader(System.in));
-				w = new PrintWriter(System.out);
-			} else if (args.length == 1) {
-				r = new BufferedReader(new FileReader(args[0]));
-				w = new PrintWriter(System.out);				
-			} else {
-				r = new BufferedReader(new FileReader(args[0]));
-				w = new PrintWriter(new FileWriter(args[1]));
-			}
-			long start = System.nanoTime();
-			doIt(r, w);
-			w.flush();
-			long stop = System.nanoTime();
-			System.err.println("Execution time: " + 10e-9 * (stop-start));
-		} catch (IOException e) {
-			System.err.println(e);
-			System.exit(-1);
-		}
-	}
 }
