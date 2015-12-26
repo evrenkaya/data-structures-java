6509f9f5679c2cfe761db1c81b315a7705513523
evrenkaya <evrenk29@gmail.com>
2015-12-26 16:15:19 -0500

    Remove main method

--- src/main/java/jds/LinearHashTable.java
+++ src/main/java/jds/LinearHashTable.java
@@ -167,52 +167,6 @@ public class LinearHashTable<T> implements USet<T> {
 	}
 	
 	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Random rand = new Random(1);
-		USet<Integer> lht = new LinearHashTable<Integer>(-1);
-		Set<Integer> s = new HashSet<Integer>();
-		int n = 1000000;
-		System.out.println("Adding");
-		for (int i = 0; i < n; i++) {
-			Integer x = rand.nextInt(n);
-			boolean rs = s.add(x);
-			boolean rlht = lht.add(x);
-			if (rs != rlht) 
-				throw new RuntimeException("Aaaaaaaaaaaaaaaah!");
-			if (s.size() != lht.size()) {
-				System.out.println(s.size());
-				System.out.println(lht.size());
-				throw new RuntimeException("Bwaaaaaaaaaaaaaaah!");
-			}
-		}
-		System.out.println("Searching");
-		for (int i = 0; i < n; i++) {
-			Integer x = rand.nextInt(n);
-			boolean rs = s.contains(x);
-			boolean rlht = lht.find(x) != null;
-			if (rs != rlht) 
-				throw new RuntimeException("Aaaaaaaaaaaaaaaah!");
-			if (s.size() != lht.size()) 
-				throw new RuntimeException("Bwaaaaaaaaaaaaaaah!");
-		}
-
-		System.out.println("Removing");
-		for (int i = 0; i < n; i++) {
-			Integer x = rand.nextInt(n);
-			boolean rs = s.remove(x);
-			boolean rlht = lht.remove(x) != null;
-			if (rs != rlht) 
-				throw new RuntimeException("Aaaaaaaaaaaaaaaah!");
-			if (s.size() != lht.size()) 
-				throw new RuntimeException("Bwaaaaaaaaaaaaaaah!");
-		}
-		s.clear();
-		lht.clear();
-	}
-	
-	/**
 	 * Table used by tabular hashing
 	 */
 	protected static int[][] tab =
