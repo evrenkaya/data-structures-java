c4b268e7e178d9cfa66471f435112eb2c8eb8245
evrenkaya <evrenk29@gmail.com>
2015-12-26 16:22:09 -0500

    Create BinaryTreeTest class

---
+++ src/main/java/jds/BinaryTreeTest.java
@@ -0,0 +1,43 @@
+package comp2402a5;
+
+public class BinaryTreeTest {
+
+	public static void main(String[] args) {
+		BinaryTree tree = new BinaryTree(5);
+		tree.r.left = new Node(6);
+		tree.r.right = new Node(7);
+		
+		tree.r.left.left = new Node(8);
+		tree.r.left.left.left = tree.nil;
+		tree.r.left.left.right = tree.nil;
+		
+		tree.r.left.right = new Node(9);
+		
+		tree.r.left.right.left = new Node(12);
+		tree.r.left.right.right = new Node(13);
+		tree.r.left.right.left.left = tree.nil;
+		tree.r.left.right.left.right = tree.nil;
+		tree.r.left.right.right.left = tree.nil;
+		tree.r.left.right.right.right = tree.nil;
+		
+		tree.r.right.left = new Node(10);
+		tree.r.right.left.right = tree.nil;
+		tree.r.right.right = new Node(11);
+		
+		tree.r.right.left.left = new Node(14);
+		tree.r.right.right.left = new Node(15);
+		tree.r.right.right.left.left = tree.nil;
+		tree.r.right.right.left.right = tree.nil;
+		tree.r.right.right.right = new Node(16);
+		tree.r.right.right.right.left = tree.nil;
+		tree.r.right.right.right.right = tree.nil;
+		
+		tree.r.right.left.left.left = tree.nil;
+		tree.r.right.left.left.right = tree.nil;
+		
+		
+		tree.inOrderNumbersNonRecursive();
+		System.out.println(tree.r.inorderN);
+	}
+
+}
