e1d945170b605c16ceeab709958ca300510af1d8
evrenkaya <evrenk29@gmail.com>
2015-12-26 16:23:14 -0500

    Create Node class

---
+++ src/main/java/jds/Node.java
@@ -0,0 +1,30 @@
+package comp2402a5;
+
+
+/**
+ * An implementation of binary trees based on the
+ * Node class from the ODS library (by P. Morin)
+ */
+public class Node {
+	public Node parent;     // parent node
+ 	public Node left;       // left child
+	public Node right;      // right child
+
+	public Integer data;    // data value stored in node
+
+	public int postorderN;  // postorder number for this node
+	public int preorderN;   // preorder number for this node
+	public int inorderN;    // inorder number for this node
+
+	public Node(){
+		parent = left = right = null;
+		data = null;
+		postorderN = preorderN = inorderN = -1;	
+	}
+
+	public Node(Integer value){
+		this();
+		data = value;
+	}
+
+}
